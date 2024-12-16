2d88fb52ac81ddd260640ccf4110304eddab69b7
unknown <kimberllee.al@gmail.com>
2024-12-16 15:20:39 -0500

    Completed the experiment section in index.js file in part 1.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+const two = 2;
+console.log(two);
+
+const five = 5;
+console.log(five);
+
+const ten = 10;
+console.log(ten);
+
+let pet = "Cranberry";
+console.log(pet);
+
+pet = "Coco";
+console.log(pet);
+
+let dessert = "Cookie";
+console.log(dessert);
+
+dessert = "Sugar"
+console.log(dessert);

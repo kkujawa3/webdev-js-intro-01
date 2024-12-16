4de60f999e17f4b73005451f41a2e91882405d32
unknown <kimberllee.al@gmail.com>
2024-12-16 15:46:22 -0500

    Completed the assignment.js in part 2 by creating a book and activity variables and updating the two functions.

--- js/assignment.js
+++ js/assignment.js
@@ -9,29 +9,29 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare a constant variable named "book" and assign
 // the name of your favorite book to that variable.
-
+const book = "Memory Man";
 
 // Declare a mutable variable named "activity" and assign
 // an activity to it.
-
+let activity = "Walking";
 
 
 function updateBookSentence() {
     // Sets the text inside the element with the id of book answer. 
     // innerText is one of many properties on a HTML element.
-    bookAnswerEl.innerText = book
+    bookAnswerEl.innerText = book;
 }
 
 function updateActivitySentence() {
     // Update this function 
     // (hint: it will be similiar to the previous function)
-
+    activityAnswerEl.innerText = activity;
 }
 
 function render() {
     // Finish writing this function
     updateBookSentence();
-
+    updateActivitySentence();
 }
 
 submissionBtn.addEventListener("click", function () {
